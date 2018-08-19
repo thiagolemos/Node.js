1501e6c8bdbf275f6d8208c7b3b99386c9993cb0
Thiago Lemos <thiagolemos95@outlook.com>
2018-08-19 17:39:18 -0300

    WebSockets com socket.io

--- config/express.js
+++ config/express.js
@@ -7,6 +7,7 @@ module.exports = function () {
     
     var app = express();
 
+    app.use(express.static('./app/public'));
     app.set('view engine', 'ejs');
     app.set('views', './app/views');
 
